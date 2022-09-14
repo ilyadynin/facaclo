15fcf34053937123cdfc5b7ff38158c7039aa437
Ilya <ilyadynin24@gmail.com>
2022-09-14 23:59:33 +0200

    added hero at the top

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -40,6 +40,10 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
+      <Hero
+        headline="SEI EINFACH FACA"
+        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
+      />
       <Grid variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
           <ProductCard
