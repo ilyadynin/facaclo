ce2a72a8bf2f2235061b04a54f83fdf35d67c7cb
Ilya <ilyadynin24@gmail.com>
2022-09-15 10:40:37 +0200

    übersetzungen

--- site/components/common/HomeAllProductsGrid/HomeAllProductsGrid.tsx
+++ site/components/common/HomeAllProductsGrid/HomeAllProductsGrid.tsx
@@ -24,7 +24,7 @@ const HomeAllProductsGrid: FC<Props> = ({
           <ul className="mb-10">
             <li className="py-1 text-base font-bold tracking-wide">
               <Link href={getCategoryPath('')}>
-                <a>All Categories</a>
+                <a>Alle Kategorien</a>
               </Link>
             </li>
             {categories.map((cat: any) => (
@@ -38,7 +38,7 @@ const HomeAllProductsGrid: FC<Props> = ({
           <ul className="">
             <li className="py-1 text-base font-bold tracking-wide">
               <Link href={getDesignerPath('')}>
-                <a>All Designers</a>
+                <a>Alle Kollektionen</a>
               </Link>
             </li>
             {brands.flatMap(({ node }: any) => (
