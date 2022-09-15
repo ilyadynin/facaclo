724b50ff3acca3b6a597d82fb31064c2cdad113d
Ilya <ilyadynin24@gmail.com>
2022-09-15 10:35:52 +0200

    übersetzungen

--- site/components/cart/CartSidebarView/CartSidebarView.tsx
+++ site/components/cart/CartSidebarView/CartSidebarView.tsx
@@ -94,20 +94,20 @@ const CartSidebarView: FC = () => {
           <div className="flex-shrink-0 px-6 py-6 sm:px-6 sticky z-20 bottom-0 w-full right-0 left-0 bg-accent-0 border-t text-sm">
             <ul className="pb-2">
               <li className="flex justify-between py-1">
-                <span>Subtotal</span>
+                <span>Summe</span>
                 <span>{subTotal}</span>
               </li>
               <li className="flex justify-between py-1">
-                <span>Taxes</span>
+                <span>Steuern</span>
                 <span>Calculated at checkout</span>
               </li>
               <li className="flex justify-between py-1">
-                <span>Shipping</span>
+                <span>Versand</span>
                 <span className="font-bold tracking-wide">FREE</span>
               </li>
             </ul>
             <div className="flex justify-between border-t border-accent-2 py-3 font-bold mb-2">
-              <span>Total</span>
+              <span>Gesamt</span>
               <span>{total}</span>
             </div>
             <div>
